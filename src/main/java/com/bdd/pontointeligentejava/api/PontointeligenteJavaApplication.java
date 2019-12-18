df3c0ebede5aed1dd2fe8846f2f59899da8431f1
brunodiego6 <bruno_diego5@hotmail.com>
2019-12-17 23:20:35 -0300

    commit inicial dos arquivos do projeto

---
+++ src/main/java/com/bdd/pontointeligentejava/api/PontointeligenteJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bdd.pontointeligentejava.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdd/pontointeligentejava/api/PontointeligenteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdd.pontointeligentejava.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
