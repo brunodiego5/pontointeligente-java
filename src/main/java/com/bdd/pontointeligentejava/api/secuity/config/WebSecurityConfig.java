6dba1eec3d480566bdbc03934ad11c7fb00fd03b
brunodiego6 <bruno_diego5@hotmail.com>
2019-12-24 02:07:50 -0300

    add controler cadastroPJ

---
+++ src/main/java/com/bdd/pontointeligentejava/api/secuity/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.bdd.pontointeligentejava.api.secuity.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic()
+			.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.csrf().disable();
+	}
+
+}
