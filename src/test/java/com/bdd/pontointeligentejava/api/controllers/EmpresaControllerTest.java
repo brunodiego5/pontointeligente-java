e1ef2fe96511a47da55eef9204b04258193ec1f5
brunodiego6 <bruno_diego5@hotmail.com>
2019-12-26 22:49:30 -0300

    add empresa controller e teste da empresa controller

---
+++ src/test/java/com/bdd/pontointeligentejava/api/controllers/EmpresaControllerTest.java
@@ -0,0 +1,81 @@
+package com.bdd.pontointeligentejava.api.controllers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.bdd.pontointeligentejava.api.entities.Empresa;
+import com.bdd.pontointeligentejava.api.services.EmpresaService;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@WithMockUser //<= para spring 2, precisei adicionar esta annotation
+public class EmpresaControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+
+	@MockBean
+	private EmpresaService empresaService;
+	
+	private static final String BUSCAR_EMPRESA_URL = "/api/empresas/cnpj/";
+	private static final Long ID = Long.valueOf(1); 
+	private static final String CNPJ = "51463645000100";
+	private static final String RAZAO_SOCIAL = "Empresa XYZ";
+	
+
+	@Test
+	public void testBuscarEmpresaCnpjInvalido() throws Exception {
+		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString())).willReturn(Optional.empty());
+		
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_URL + CNPJ).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.errors").value("Empresa não encontrada para o CNPJ " + CNPJ));
+	}
+	
+	@Test
+	public void testBuscarEmpresaCnpjValido() throws Exception {
+		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString()))
+			.willReturn(Optional.of(this.obterDadosEmpresa()));
+		
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_URL + CNPJ).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.id").value(ID))
+				.andExpect(jsonPath("$.data.razaoSocial", equalTo(RAZAO_SOCIAL)))
+				.andExpect(jsonPath("$.data.cnpj", equalTo(CNPJ)))
+				.andExpect(jsonPath("$.errors").isEmpty());
+		
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		
+		empresa.setId(ID);
+		empresa.setCnpj(CNPJ);
+		empresa.setRazaoSocial(RAZAO_SOCIAL);
+		
+		return empresa;
+	}
+	
+
+}
