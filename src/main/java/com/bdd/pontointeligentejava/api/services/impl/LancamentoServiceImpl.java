9ef9004d86ef2b94ce65c89ba64adadc641aff07
brunodiego6 <bruno_diego5@hotmail.com>
2019-12-31 18:15:24 -0300

    adicionado o ehCache

--- src/main/java/com/bdd/pontointeligentejava/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/bdd/pontointeligentejava/api/services/impl/LancamentoServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -21,27 +23,25 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
-	@Override
 	public Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
 		log.info("Buscando lancamentos para o funcionario id {}", funcionarioId);
 		
 		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
 	}
 
-	@Override
+	@Cacheable("lancamentoPorId") /*a primeira vez busca no banco e add o resultado no cache, as proximas a busca lê o cache (3600 s) */
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando um lancamento pelo id {}", id);
 		// return this.lancamentoRepository.findOne(id); <= para spring 1.5
 		return this.lancamentoRepository.findById(id);
 	}
 
-	@Override
+	@CachePut("lancamentoPorId") /*mantem o cache atualizado*/
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo o lançamento {} ", lancamento);
 		return this.lancamentoRepository.save(lancamento);
 	}
 
-	@Override
 	public void remover(Long id) {
 		log.info("removendo o lancamento id {} ", id);
 		//this.lancamentoRepository.delete(id); <= para spring 1.5
